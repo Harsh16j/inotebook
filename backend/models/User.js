ee711ee78c51886b2585a74992da7892e7963c7d
Harsh <harsh.jethwani16@gmail.com>
2022-08-15 01:20:37 -0500

    Express validation incorporated

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,8 +17,9 @@ const userSchema = new Schema({
     },
     date:{
         type:Date,
-        default:Date.now
+        default:Date.now,
     }
   });
-
-  module.exports=mongoose.model("user",userSchema);
+const User=mongoose.model("user",userSchema);
+User.createIndexes();
+module.exports=User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,19 +1,35 @@
 const express=require("express");
 const User=require("../models/User")
-
+const { body, validationResult } = require('express-validator');
 
 const router=express.Router();
 
 // Create a user using POST "/api/auth". Doesn't require Auth
-router.post('/',(req,res)=>{
-    obj={
-        a:1,
-        b:"23"
+router.post('/',
+body('name').isLength({min:1}),
+body('email').isEmail(),
+body('password','Password should be of atleast 6 characters').isLength({ min: 6 })
+,(req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body)
-    const user=User(req.body)
-    user.save()
-    res.send(req.body);
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err=>
+        {console.log(err);
+            res.json({error:"Error in updating documents in mongoDB database",
+        message:err.message});
+    });
+    
+    // const user=User(req.body)
+    // user.save()
+    // res.send(req.body);
 });
 
 module.exports=router
