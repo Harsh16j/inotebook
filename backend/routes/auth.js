32703b6dae16655c7d6ef42f65c20e0ddf745f80
Harsh <harsh.jethwani16@gmail.com>
2022-08-14 20:00:30 -0500

    Models and Routes Post request to the DB added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,19 @@
 const express=require("express");
+const User=require("../models/User")
+
 
 const router=express.Router();
 
-router.get('/',(req,res)=>{
+// Create a user using POST "/api/auth". Doesn't require Auth
+router.post('/',(req,res)=>{
     obj={
         a:1,
         b:"23"
     }
     console.log(req.body)
-    res.json(obj);
+    const user=User(req.body)
+    user.save()
+    res.send(req.body);
 });
 
 module.exports=router
