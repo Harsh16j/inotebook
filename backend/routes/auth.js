83f91d7c3c7b57ca0dfd028e632188d0a17a09c6
Harsh <harsh.jethwani16@gmail.com>
2022-08-17 00:48:43 -0500

    Login route created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -36,7 +36,7 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       // Generating hashed password with salt
       const secPass = await bcrypt.hash(req.body.password, salt);
-      
+
       //Storing the new user data in the database
       user = await User.create({
         name: req.body.name,
@@ -45,18 +45,18 @@ router.post(
       });
 
       // data with document id created to be used to create authorization token
-      const data={
-        user:{
-            id:user.id
-        }
-      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
       // Authorization token created with data and JWT SECRET CODE
-      const authToken=jwt.sign(data,JWT_SECRET)
+      const authToken = jwt.sign(data, JWT_SECRET);
       // Authorization token sent back to the user
       res.json(authToken); // same as res.json({authToken:authToken})
-    }catch (error) {
+    } catch (error) {
       console.error(error.message);
-      res.status(500).send("some error occured");
+      res.status(500).send("Internal server errro");
     }
 
     //old method 2
@@ -75,6 +75,47 @@ router.post(
     // const user=User(req.body)
     // user.save()
     // res.send(req.body);
+  }
+);
+
+// Authenticate a user using POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  body("email").isEmail(),
+  body("password", "Password cannot be blank").exists(),
+  async (req, res) => {
+    // If there are errors, return bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({email:email});
+      //If user does not exist
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      // When wrong password is entered
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+      // Payload is the data to be returned
+      const payload = {
+        user: { user: user.id },
+      };
+      // Creating and sending authorization token
+      const authToken = jwt.sign(payload, JWT_SECRET);
+      res.json(authToken);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 module.exports = router;
