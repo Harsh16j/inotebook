abe7210ec3790224afe6c715b51042b474328b6c
Harsh <harsh.jethwani16@gmail.com>
2022-08-20 16:18:54 -0500

    Setting up bootstrap, navbar and Router

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>This is home</div>
+      <h1>This is my iNotebook</h1>
+    </>
+  );
+};
+
+export default Home;
